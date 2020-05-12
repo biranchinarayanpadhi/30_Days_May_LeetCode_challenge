eb131ec7b19d18c79e1780a63d95915bb70d0fe6
DESKTOP-4QSMR20\Biranchi Padhi <biranchi.padhi@machstatz.com>
2020-05-13 00:07:33 +0530

    12th Day Challenge

---
+++ single_element_in_sorted_Array.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def singleNonDuplicate(self, arr):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left=0
+        right=len(arr)-1
+        
+        while left<=right:
+            mid=(left+right)//2
+            
+            print(left,right,mid,mid-1,mid+1) 
+            
+            if mid-1>=0 and mid+1<=len(arr)-1:
+            
+                if arr[mid]!=arr[mid-1] and arr[mid]!=arr[mid+1]:
+                    return arr[mid]
+
+                elif arr[mid] == arr[mid-1]:
+                    if (mid+1)%2==0:
+                        left=mid+1
+                    else:
+                        right=mid-1
+                else:
+                    if (mid)%2==0:
+                        left=mid+1
+                    else:
+                        right=mid-1
+            else:
+                return arr[mid]            
